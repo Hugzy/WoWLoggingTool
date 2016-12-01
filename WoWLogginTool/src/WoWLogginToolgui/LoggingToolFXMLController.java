d3549103893880aecf04d1bcbcb4f1ecace94bd7
Hugzy <frede.madsen3@gmail.com>
2016-12-01 19:58:30 +0100

    fxml is now in the same class as controller

--- WoWLogginTool/src/WoWLogginToolgui/LoggingToolFXMLController.java
+++ WoWLogginTool/src/WoWLogginToolgui/LoggingToolFXMLController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package loggingtool;
+package WoWLogginToolgui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
